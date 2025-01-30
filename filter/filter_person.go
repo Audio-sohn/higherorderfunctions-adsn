b29ec9e387a201a1c2b20268c5e043885e0581e5
Reiner Huechting <dev@reinerhuechting.de>
2025-01-30 18:14:29 +0100

    Remove hints.

--- filter/filter_person.go
+++ filter/filter_person.go
@@ -9,21 +9,14 @@ type Person struct {
 // FilterMinAge liefert eine neue Liste, die nur die Personen der ursprünglichen Liste enthält,
 // die mindestens das angegebene Alter haben.
 func FilterMinAge(list []Person, minAge int) []Person {
-	// HINWEIS: Verwenden Sie die Funktion FilterList.
-	// Übergeben Sie ihr eine Funktion, die für eine Person p
-	// überprüft, ob sie mindestens das Alter minAge hat.
-	return FilterList(list, func(p Person) bool {
-		return p.Age >= minAge
-	})
+	// TODO
 }
 
 // FilterLongNames erwartet eine Liste von Personen und eine Mindestlänge.
 // FilterLongNames liefert eine neue Liste, die nur die Personen der ursprünglichen Liste enthält,
 // deren Name mindestens die angegebene Länge hat.
 func FilterLongNames(list []Person, minLength int) []Person {
-	// HINWEIS: Verwenden Sie die Funktion FilterList.
-	// Übergeben Sie ihr eine Funktion, die für eine Person p
-	// überprüft, ob ihr Name wenigstens minLength Zeichen hat.
+	// TODO
 	return []Person{}
 }
 
@@ -31,9 +24,7 @@ func FilterLongNames(list []Person, minLength int) []Person {
 // FilterNamePrefix liefert eine neue Liste, die nur die Personen der ursprünglichen Liste enthält,
 // deren Name mit dem angegebenen Präfix beginnt.
 func FilterNamePrefix(list []Person, prefix string) []Person {
-	// HINWEIS: Verwenden Sie die Funktion FilterList.
-	// Übergeben Sie ihr eine Funktion, die für eine Person p
-	// überprüft, ob ihr Name mit prefix beginnt.
+	// TODO
 	return []Person{}
 }
 
@@ -41,9 +32,7 @@ func FilterNamePrefix(list []Person, prefix string) []Person {
 // FilterChildren liefert eine neue Liste, die nur die Personen der ursprünglichen Liste enthält,
 // die höchstens 13 Jahre alt sind.
 func FilterChildren(list []Person) []Person {
-	// HINWEIS: Verwenden Sie die Funktion FilterList.
-	// Übergeben Sie ihr eine Funktion, die für eine Person p
-	// überprüft, ob ihr Alter höchstens 13 ist.
+	// TODO
 	return []Person{}
 }
 
@@ -51,6 +40,6 @@ func FilterChildren(list []Person) []Person {
 // FilterChildrenWithLongNames liefert eine neue Liste, die Personen aus der ursprünglichen Liste enthält,
 // die höchstens 13 Jahre alt sind und deren Name mindestens die angegebene Länge hat.
 func FilterChildrenWithLongNames(list []Person, minLength int) []Person {
-	// HINWEIS: Kombinieren Sie geeignete Funktionen, die Sie bereits geschrieben haben.
+	// TODO
 	return []Person{}
 }
